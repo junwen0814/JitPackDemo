2f735116c3f81d39356fd283eb2c425e48db19d7
junwen <344176791@qq.com>
2016-11-02 17:38:31 +0800

    ddd

---
+++ jitmodule/src/main/java/library/junwen/com/jitmodule/TestUtils.java
@@ -0,0 +1,17 @@
+package library.junwen.com.jitmodule;
+
+import android.util.Log;
+
+/**
+ * 描述：
+ * 作者：卜俊文
+ * 创建：2016/11/2 17:37
+ * 邮箱：344176791@qq.com
+ */
+public class TestUtils {
+
+    public void print() {
+        Log.e("TestUtils", "test");
+    }
+
+}
